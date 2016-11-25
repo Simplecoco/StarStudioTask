8e9ccfaebb3e0f252bc0e29dad730c03a58f8e7a
Simplecoco <dkdsh@163.com>
2016-11-25 15:28:35 +0800

    task16

--- task16/task16.js
+++ task16/task16.js
@@ -1,20 +1,8 @@
 /**
  * Created by 辛珀 on 2016/11/8.
  */
-/**
- * aqiData，存储用户输入的空气指数数据
- * 示例格式：
- * aqiData = {
- *    "北京": 90,
- *    "上海": 40
- * };
- */
-var aqiData = {};
 
-/**
- * 从用户输入中获取数据，向aqiData中增加一条数据
- * 然后渲染aqi-list列表，增加新增的数据
- */
+var aqiData = {};
 
 window.onload=function(){
     var addBtn=document.getElementById("add-btn");
@@ -47,10 +35,6 @@ function addAqiData() {
     renderAqiList(aqiData);  //使用这个函数时记得带参数。。。
 }
 
-
-/**
- * 渲染aqi-table表格
- */
 function renderAqiList(aqiData) {
     var aqiTable=document.getElementById("aqi-table");
     var newTable=document.createElement("table");
@@ -84,19 +68,10 @@ function renderAqiList(aqiData) {
 
 }
 
-
-/**
- * 点击add-btn时的处理逻辑
- * 获取用户输入，更新数据，并进行页面呈现的更新
- */
 function addBtnHandle() {
     addAqiData();              //渲染函数在addAqiData合理之后才执行，所以放到它里面
 }
 
-/**
- * 点击各个删除按钮的时候的处理逻辑
- * 获取哪个城市数据被删，删除数据，更新表格显示
- */
 function delBtnHandle(event) {          //给删除按钮的父元素table绑定了点击事件，点击删除按钮时，点击按钮事件冒泡到它的父元素上，委托父元素代它执行
     if(event.target.nodeName=="BUTTON"){   //必须判断点击的元素类型，否则table的子元素都会执行这个点击事件
         var city=event.target.parentNode.parentNode.firstChild.innerText;
